69cee17eb7cf841ded6dcc596bbef62e6206c950
mugesh os <mugesh@oceansoftteam.in>
2026-01-22 18:45:13 +0530

    chnages

---
+++ src/entity/Training.ts
@@ -0,0 +1,68 @@
+import {
+  Entity,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { ObjectId } from "mongodb";
+import { TrainingStatus } from "../enum/TrainingStatus";
+
+@Entity()
+export class Training {
+  @ObjectIdColumn()
+  id: ObjectId;
+
+  @Column()
+  chapterIds: ObjectId[];
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  trainerIds: ObjectId[];
+
+  @Column()
+  trainingDateTime: Date;
+
+  @Column()
+  lastDateForApply: Date;
+
+  @Column()
+  duration: string;
+
+  @Column()
+  mode: "online" | "in-person";
+
+  @Column()
+  locationOrLink: string;
+
+  @Column()
+  maxAllowed: number;
+
+  @Column({
+    default: TrainingStatus.UPCOMING
+  })
+  status: TrainingStatus;
+
+  @Column({ default: 1 })
+  isActive: number;
+
+  @Column({ default: 0 })
+  isDelete: number;
+
+  @Column()
+  createdBy: ObjectId;
+
+  @Column()
+  updatedBy: ObjectId;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
